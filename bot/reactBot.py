1bcc2f5c662f8d8f55bc486c1e18f4d8864d1a91
HyunJunSon <jjang3a@gmail.com>
2023-11-30 17:44:16 +0900

    [refactor] 전체적인 구조 리팩토링

--- bot/reactBot.py
+++ bot/reactBot.py
@@ -18,114 +18,138 @@ SERVICE_ACCOUNT_FILE = '../splendid-myth-353301-a63d721b9519.json'
 SPREADSHEET_ID = os.getenv("GOOGLE_SHEET_ID")
 RANGE_NAME = '시트1!A2:G30'
 
+class SlackEventHandler:
+    def __init__(self, bot):
+        self.bot = bot
+        self.greeting = ["안녕", "하이", "방가"]
+        self.member = ["병룡님", "민협님", "영롱님", 
+                       "원상님", "찬규님", "현지님", 
+                       "찬욱님", "석희님", "민우님", 
+                       "현준님", "예진님", "종민님", 
+                       "소현님", "무룡님", "윤선님", 
+                       "종찬님", "호윤님", "지용님", 
+                       "형준님", "주광님"]
+        
+        self.message_dict = {
+            'how_to_use': 
+                """
+                    안녕하세요! 커널 360봇 입니다!
+                    멘션해주셔서 감사합니다.
+                    업데이트 및 관련 요청 문의는 석희님 원상님 현준님을 멘션해주세요.
+               """
+            ,
+            'greeting': "안녕하세요~! 반갑습니다 커널 봇입니다.",
+        }
 
-greetings = ["안녕", "하이", "방가"]
+    def _get_event_details(self, slack_event):
+        
+        channel = slack_event["event"]["channel"]
+        message_ts = slack_event["event"]["event_ts"]
+        return channel, message_ts
 
-def show_how_to_use(event_type, slack_event):
-    channel = slack_event["event"]["channel"]
-    message = slack_event["event"]["event_ts"]
-    text = "안녕하세요! 커널 360봇 입니다!\n멘션해주셔서 감사합니다.\n업데이트 및 관련 요청 문의는 석희님 원상님 현준님을 멘션해주세요."
-    myBot.post_message(channel, text)
-    message = "[%s] 이벤트 핸들러를 찾을 수 없습니다." % event_type
-    return make_response(message, 200, {"X-Slack-No-Retry": 1})
 
-def say_hello(event_type, slack_event):
-    channel = slack_event["event"]["channel"]
-    message = slack_event["event"]["event_ts"]
-    text = "안녕하세요~! 반갑습니다 커널 봇입니다."
-    myBot.post_message(channel, text)
-    message = "[%s] 이벤트 핸들러를 찾을 수 없습니다." % event_type
-    return make_response(message, 200, {"X-Slack-No-Retry": 1})
+    def send_message(self, event_type, slack_event, message_type):
+        
+        channel, message_ts = self._get_event_details(slack_event)
+        self.bot.post_message(channel, self.message_dict.get(message_type, "메세지타입이 확인되지 않았습니다."))
+        return make_response(f"{event_type} 이벤트 핸들러를 찾을 수 없습니다.", 200, {"X-Slack-No-Retry": 1})
 
-members = ["병룡님", "민협님", "영롱님", "원상님", "찬규님", "현지님", "찬욱님", "석희님", "민우님", "현준님", "예진님", "종민님", "소현님", "무룡님", "윤선님", "종찬님", "호윤님", "지용님", "형준님", "주광님"]
 
-def randomMember(event_type, slack_event, num):
-    channel = slack_event["event"]["channel"]
-    message = slack_event["event"]["event_ts"]
-    num = int(num)
-    selected_members = random.sample(members, min(num, len(members)))
-    text = ', '.join(selected_members)
-    myBot.post_message(channel, text)
+    def random_member(self, event_type, slack_event, num):
+        
+        channel, message_ts = self._get_event_details(slack_event)
 
-    message = "[%s] 이벤트 핸들러를 찾을 수 없습니다." % event_type
-    return make_response(message, 200, {"X-Slack-No-Retry": 1})
+        selected_members = random.sample(self.member, min(int(num), len(self.member)))
+        text = ', '.join(selected_members)
+        self.bot.post_message(channel, text)
+
+        return make_response(f"{event_type} 이벤트 핸들러를 찾을 수 없습니다." , 200, {"X-Slack-No-Retry": 1})
     
-def randomRestaurant(event_type, slack_event, category, count):
-    channel = slack_event["event"]["channel"]
-    message = slack_event["event"]["event_ts"]
-    text = ""
-    values = get_spreadsheet_data(SERVICE_ACCOUNT_FILE, SPREADSHEET_ID, RANGE_NAME)
-    recommendation = Recommendation(values)
+    
+    def _parse_distance(self, category_lower):
+        if "km" in category_lower:
+            return float(category_lower.replace("km", ""))
+        elif "m" in category_lower:
+            return float(category_lower.replace("m", "")) / 1000
+        return None
+    
+    
+    def random_restaurant(self, event_type, slack_event, category, count):
+        values = get_spreadsheet_data(SERVICE_ACCOUNT_FILE, SPREADSHEET_ID, RANGE_NAME)
+        recommendation = Recommendation(values)
+        
+        distance = self._parse_distance(category.lower())
+        
+        restaurant_methods = {
+            'distance': lambda: recommendation.get_close_restaurant(distance, int(count)),
+            '무작위': lambda: recommendation.get_random(int(count)),
+            'default': lambda: recommendation.get_categorized_restaurant(category, int(count))
+        }
 
-    category_lower = category.lower()
+        method = restaurant_methods.get('distance' if distance is not None else category.lower(), restaurant_methods['default'])
+        restaurants = method()
 
-    if "km" in category_lower:
-        distance = float(category_lower.replace("km", ""))
-    elif "m" in category_lower and "km" not in category_lower:
-        distance = float(category_lower.replace("m", "")) / 1000
+        text = "\n".join([OutputRestaurant(row.tolist()).__str__() for _, row in restaurants.iterrows()])
 
-    if "km" in category_lower or ("m" in category_lower and "km" not in category_lower):
-        for rec_string in [OutputRestaurant(row.tolist()).__str__() for idx, row in recommendation.get_close_restaurant(distance, int(count)).iterrows()]:
-            text += rec_string + "\n"
+        channel, _ = self._get_event_details(slack_event)
 
-    elif category_lower == "무작위":
-        for rec_string in [OutputRestaurant(row.tolist()).__str__() for idx, row in recommendation.get_random(int(count)).iterrows()]:
-            text += rec_string + "\n"
+        if text:
+            self.bot.post_message(channel, text)
+        else:
+            message = f"[{category}] 적절한 카테고리가 없습니다."
+            self.bot.post_message(channel, message)
 
-    else:
-        for rec_string in [OutputRestaurant(row.tolist()).__str__() for idx, row in recommendation.get_categorized_restaurant(category, int(count)).iterrows()]:
-            text += rec_string + "\n"
+        return make_response(message, 200, {"X-Slack-No-Retry": 1})
 
-    if text:
-        myBot.post_message(channel, text)
-    else:
-        message = "[%s] 적절한 카테고리를 찾을 수 없습니다." % category
-        myBot.post_message(channel, message)
 
-    return make_response(message, 200, {"X-Slack-No-Retry": 1})
+    @staticmethod
+    def catch_restaurant(text):
+        pattern = r"식당추천\s+(\S+)\s+(\d+)군데"
+        match = re.search(pattern, text)
 
-def catch_restaurant(text):
-    pattern = r"식당추천\s+(\S+)\s+(\d+)군데"
-    match = re.search(pattern, text)
+        if match:
+            category = match.group(1)
+            count = int(match.group(2))
+            return category, count
+        else:
+            return None,None
 
-    if match:
-        category = match.group(1)
-        count = int(match.group(2))
-        return category, count
-    else:
-        return
 
-def sendQr(slack_event):
-    channel = slack_event["event"]["channel"]
-    message = slack_event["event"]["event_ts"]
-    myBot.post_qr_image(channel)
-    return make_response(message, 200, {"X-Slack-No-Retry": 1})
+    def sendQr(self, slack_event):
+        
+        channel = slack_event["event"]["channel"]
+        message = slack_event["event"]["event_ts"]
+        self.bot.post_qr_image(channel)
+        return make_response(message, 200, {"X-Slack-No-Retry": 1})
 
-def event_handler(event_type, slack_event):
-    print(slack_event)
 
-    if(event_type == "app_mention"): 
-        text = slack_event["event"]["text"]
-        if ("식당추천" in text):
-            category, count = catch_restaurant(text)
-            return randomRestaurant(event_type, slack_event, category, count)
+    def handle_app_mention(self, text, event_type, slack_event):
+        
+        if "식당추천" in text:
+            category, count = self.catch_restaurant(text)
+            return self.random_restaurant(event_type, slack_event, category, count)
         if re.search(r"추첨\s+\d+", text):
             num = re.search(r"추첨\s+(\d+)", text).group(1)
-            return randomMember(event_type, slack_event, num)
-        if ("qr" in text):
-            return sendQr(slack_event)
-        if any(greeting in text for greeting in greetings):
-                return say_hello(event_type, slack_event) 
-        return show_how_to_use(event_type, slack_event)
+            return self.random_member(event_type, slack_event, num)
+        if "qr" in text:
+            return self.send_qr(slack_event)
+        if any(greeting in text for greeting in self.greeting):
+            return self.say_hello(event_type, slack_event)
+        return self.show_how_to_use(event_type, slack_event)
 
-    channel = slack_event["event"]["channel"]
-    message = slack_event["event"]["event_ts"]
-    test = "안녕하세요. 스레드 봇이 알 수 없는 요청입니다\n github에 issue report를 남겨주세요"
-    myBot.post_message_in_thread(channel, message, test)
 
-    message = "[%s] 이벤트 핸들러를 찾을 수 없습니다." % event_type
-    return make_response(message, 200, {"X-Slack-No-Retry": 1})
+    def event_handler(self, event_type, slack_event):
+        
+        print(slack_event)
+        if event_type == "app_mention":
+            return self.handle_app_mention(slack_event["event"]["text"], event_type, slack_event)
 
+        response_text = f"안녕하세요. 스레드 봇이 알 수 없는 요청입니다 github에 issue report를 남겨주세요"
+        channel, _ = self._get_event_details(slack_event)
+        self.bot.post_message_in_thread(channel, slack_event["event"]["event_ts"], response_text)
+        return make_response(response_text, 200, {"X-Slack-No-Retry": 1})
+    
+    
 @app.route("/slack", methods=["GET", "POST"])
 def hears():
     slack_event = json.loads(request.data)
@@ -136,7 +160,8 @@ def hears():
     
     if "event" in slack_event:
         event_type = slack_event["event"]["type"]
-        return event_handler(event_type, slack_event)
+        slack_event_handler = SlackEventHandler(myBot)
+        return slack_event_handler.event_handler(event_type, slack_event)
     
     return make_response("슬랙 요청에 이벤트가 없습니다.", 404, {"X-Slack-No-Retry": 1})
 
